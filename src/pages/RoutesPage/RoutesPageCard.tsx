5dfdb9b20b2bfa99165908a33f29e5c534737507
mrtnsund <40366483+mrtnsund@users.noreply.github.com>
2020-07-22 11:08:19 +0200

    added destination icons for routes

--- src/pages/RoutesPage/RoutesPageCard.tsx
+++ src/pages/RoutesPage/RoutesPageCard.tsx
@@ -1,6 +1,6 @@
-import { IonCard, IonCardHeader, IonItem, IonIcon, IonCardSubtitle, IonCardTitle, IonCardContent } from "@ionic/react";
+import { IonCard, IonCardHeader, IonItem, IonIcon, IonCardSubtitle, IonCardContent } from "@ionic/react";
 import React from "react";
-import { archiveSharp, pencilSharp, trashSharp, caretDownSharp } from "ionicons/icons";
+import { archiveSharp, pencilSharp, trashSharp, caretDownSharp, flagOutline, pinOutline, ellipsisVertical, pin, flag } from "ionicons/icons";
 
 const RoutesPageCard = () => {
     return (
@@ -11,17 +11,24 @@ const RoutesPageCard = () => {
             <IonIcon slot="end" icon={pencilSharp} className="editIcon"/>
             <IonIcon slot="end" icon={trashSharp} className="deleteIcon"/>
           <IonCardSubtitle>
-            <p>10 - 14 july</p>
           </IonCardSubtitle>
           </IonItem>
           <IonItem lines="none">
-          <IonCardTitle>Første tur</IonCardTitle>
+            <IonIcon slot="start" icon={pin} className="pinIcon"/>
+            <p className="destination">Bergen</p>
+          </IonItem>
+          <IonItem lines="none">
+            <IonIcon slot="start" icon={ellipsisVertical} className="ellipsisIcon"/> 
+          </IonItem>
+          <IonItem lines="none">
+            <IonIcon slot="start" icon={flag} className="flagIcon"/>
+            <p className="destination">Stavanger</p>
           </IonItem>
         </IonCardHeader>
 
         <IonCardContent>
           <IonItem lines="none">
-          <IonIcon slot="end" icon={caretDownSharp} className="archiveIcon"/>
+          <IonIcon slot="end" icon={caretDownSharp} className="expandIcon"/>
 
           </IonItem>
         </IonCardContent>
