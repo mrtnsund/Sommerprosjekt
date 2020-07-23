49edee2d0e2618e36b34931a3e1d36890e77884f
mrtnsund <40366483+mrtnsund@users.noreply.github.com>
2020-07-23 10:39:56 +0200

    added logic to expand button

--- src/pages/RoutesPage/RoutesPageCard.tsx
+++ src/pages/RoutesPage/RoutesPageCard.tsx
@@ -1,38 +1,80 @@
-import { IonCard, IonCardHeader, IonItem, IonIcon, IonCardSubtitle, IonCardContent } from "@ionic/react";
-import React from "react";
-import { archiveSharp, pencilSharp, trashSharp, caretDownSharp, flagOutline, pinOutline, ellipsisVertical, pin, flag, ellipsisHorizontal } from "ionicons/icons";
+import {
+  IonCard,
+  IonCardHeader,
+  IonItem,
+  IonIcon,
+  IonCardSubtitle,
+  IonCardContent,
+} from "@ionic/react";
+import React, { useState } from "react";
+import {
+  archiveSharp,
+  pencilSharp,
+  trashSharp,
+  caretDownSharp,
+  flagOutline,
+  pinOutline,
+  ellipsisVertical,
+  pin,
+  flag,
+  ellipsisHorizontal,
+  caretUpSharp,
+} from "ionicons/icons";
 
 const RoutesPageCard = () => {
-    return (
-        <IonCard>
-        <IonCardHeader>
+  const [expand, setExpand] = useState(false);
+  const expandCard = () => {
+    if (expand) {
+      return (
+        <div>
+          <IonItem lines="none"></IonItem>
+          <IonItem lines="none"></IonItem>
           <IonItem lines="none">
-            <IonIcon slot="end" icon={archiveSharp} className="archiveIcon"/>
-            <IonIcon slot="end" icon={pencilSharp} className="editIcon"/>
-            <IonIcon slot="end" icon={trashSharp} className="deleteIcon"/>
+            <IonIcon
+              slot="end"
+              icon={caretUpSharp}
+              className="expandIcon"
+              onClick={() => setExpand(false)}
+            />
+          </IonItem>
+        </div>
+      );
+    } else {
+      return (
+        <IonItem lines="none">
+          <IonIcon
+            slot="end"
+            icon={caretDownSharp}
+            className="expandIcon"
+            onClick={() => setExpand(true)}
+          />
+        </IonItem>
+      );
+    }
+  };
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonItem lines="none">
+          <IonIcon slot="end" icon={archiveSharp} className="archiveIcon" />
+          <IonIcon slot="end" icon={pencilSharp} className="editIcon" />
+          <IonIcon slot="end" icon={trashSharp} className="deleteIcon" />
           <IonCardSubtitle>
-          <IonItem lines="none">
-              <IonIcon icon={pin} className="pinIcon"/>
+            <IonItem lines="none">
+              <IonIcon icon={pin} className="pinIcon" />
               <p className="destinationTo">Bergen</p>
 
-              <IonIcon icon={ellipsisHorizontal} className="ellipsisIcon"/> 
+              <IonIcon icon={ellipsisHorizontal} className="ellipsisIcon" />
               <p className="destinationFrom">Stavanger</p>
-              <IonIcon icon={flag} className="flagIcon"/>
-
-          </IonItem>
+              <IonIcon icon={flag} className="flagIcon" />
+            </IonItem>
           </IonCardSubtitle>
-          </IonItem>
+        </IonItem>
+      </IonCardHeader>
 
-        </IonCardHeader>
-
-        <IonCardContent>
-          <IonItem lines="none">
-          <IonIcon slot="end" icon={caretDownSharp} className="expandIcon"/>
-
-          </IonItem>
-        </IonCardContent>
-      </IonCard>
-    )
-}
+      <IonCardContent>{expandCard()}</IonCardContent>
+    </IonCard>
+  );
+};
 
 export default RoutesPageCard;
