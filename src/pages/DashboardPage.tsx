1e6809e22131c92be0903308da0807e628703d92
enorvang <espen.norvang@gmail.com>
2020-08-17 21:47:30 +0200

    removed import.

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -9,7 +9,6 @@ import {
   IonAvatar,
 } from "@ionic/react";
 import React from "react";
-import Example from "../components/IonItemGroupExample";
 
 const DashboardPage: React.FC = () => {
   return (
@@ -24,9 +23,11 @@ const DashboardPage: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <h1>Welcome to the Dashboard</h1>
-        <IonAvatar>
-          <img src="https://akamai.vgc.no/v2/images/9a407910-0876-4fce-8bca-14fe2865ec3a?fit=crop&h=2000&w=1831&s=b6bd65a63095b2a40a8cf3e6632e54865b576b82"/>
-        </IonAvatar>
+        <div style={{ marginLeft: "5%" }}>
+          <IonAvatar>
+            <img src="https://akamai.vgc.no/v2/images/9a407910-0876-4fce-8bca-14fe2865ec3a?fit=crop&h=2000&w=1831&s=b6bd65a63095b2a40a8cf3e6632e54865b576b82" />
+          </IonAvatar>
+        </div>
       </IonContent>
     </IonPage>
   );
