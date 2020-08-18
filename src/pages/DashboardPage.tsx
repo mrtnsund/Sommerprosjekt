f5e6053d491e93c6980cb5bb776f682695bbdf9f
mrtnsund <40366483+mrtnsund@users.noreply.github.com>
2020-08-18 12:31:56 +0200

    added cards on dashboard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -7,13 +7,16 @@ import {
   IonToolbar,
   IonButtons,
   IonAvatar,
+  IonGrid,
+  IonRow,
+  IonCol,
 } from "@ionic/react";
+import './DashboardPage.css';
 import React from "react";
 
 const DashboardPage: React.FC = () => {
   return (
     <IonPage>
-      <IonContent>
         <IonHeader>
           <IonToolbar>
             <IonButtons slot="start">
@@ -22,12 +25,20 @@ const DashboardPage: React.FC = () => {
             <IonTitle size="large">Dashboard</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <h1>Welcome to the Dashboard</h1>
-        <div style={{ marginLeft: "5%" }}>
-          <IonAvatar>
-            <img src="https://akamai.vgc.no/v2/images/9a407910-0876-4fce-8bca-14fe2865ec3a?fit=crop&h=2000&w=1831&s=b6bd65a63095b2a40a8cf3e6632e54865b576b82" alt=""/>
-          </IonAvatar>
-        </div>
+
+      <IonContent>
+        <IonGrid className="cardsContainer">
+          <IonRow className="cards">
+            <IonCol className="card en">
+              My trips
+            </IonCol>
+            <IonCol className="card to">Give recommendations</IonCol>
+          </IonRow>
+          <IonRow className="cards">
+            <IonCol className="card tre">Placeholder</IonCol>
+            <IonCol className="card fire">Placeholder</IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
